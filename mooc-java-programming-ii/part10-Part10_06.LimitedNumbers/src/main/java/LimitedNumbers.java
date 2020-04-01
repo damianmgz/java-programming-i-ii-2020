ad93b265637ea3acbcb199f215e47a0dbe313abe
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-01 17:46:20 +0200

    Finish part 10 exercise 6

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,20 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while(true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input < 0) {
+                break;
+            }
+            
+            numbers.add(input);
+        }
+        
+        numbers.stream()
+                .filter(number -> (number >= 0) && (number <= 5))
+                .forEach(number -> System.out.println(number)); 
     }
 }
