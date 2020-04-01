ddeb577a4ff5898c2c88c734722406c95ce980c7
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-01 16:54:37 +0200

    Finish part 10 exercise 4

--- mooc-java-programming-ii/part10-Part10_04.Divisible/src/main/java/Divisible.java
+++ mooc-java-programming-ii/part10-Part10_04.Divisible/src/main/java/Divisible.java
@@ -15,11 +15,14 @@ public class Divisible {
         ArrayList<Integer> divisible = divisible(numbers);
 
         divisible.stream()
-                .forEach(luku -> System.out.println(luku));
+                .forEach(number -> System.out.println(number));
     }
 
     public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
-        return new ArrayList<>();
+        ArrayList<Integer> divisible = numbers.stream()
+                .filter(value -> (value % 2 == 0) || (value % 3 == 0) || (value % 5 == 0))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return divisible;
     }
 
 }
