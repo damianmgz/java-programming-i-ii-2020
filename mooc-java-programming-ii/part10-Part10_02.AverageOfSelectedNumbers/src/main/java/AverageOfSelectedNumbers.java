e489afe5621e69ba9ba0842ce5d2c4e8895e2ab4
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-01 13:15:22 +0200

    Finish part 10 exercise 2

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -7,6 +7,37 @@ public class AverageOfSelectedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // toteuta ohjelmasi tänne
+        ArrayList<String> list = new ArrayList<>();
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String selectAverage = scanner.nextLine();
+
+        if (selectAverage.equals("n")) {
+            double average = list.stream()
+                    .mapToInt(value -> Integer.valueOf(value))
+                    .filter(value -> (value < 0))
+                    .average()
+                    .getAsDouble();
+            System.out.println("Average of the negative numbers: " + average);
+        }
+
+        if (selectAverage.equals("p")) {
+            double average = list.stream()
+                    .mapToInt(value -> Integer.valueOf(value))
+                    .filter(value -> (value > 0))
+                    .average()
+                    .getAsDouble();
+            System.out.println("Average of the positive numbers: " + average);
+        }
     }
 }
