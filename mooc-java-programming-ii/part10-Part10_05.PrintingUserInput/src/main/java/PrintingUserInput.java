4bf3c4dca83e4f1fefc30e661e6ecd1bef980886
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-01 17:36:36 +0200

    Finish part 10 exercise 5

--- mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,18 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        ArrayList<String> strings = new ArrayList<>();
+        
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            strings.add(input);
+        }
+        
+        strings.stream()
+                .forEach(line -> System.out.println(line));
     }
 }
