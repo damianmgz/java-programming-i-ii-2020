f01e460fdd060c4d0827e1c5a2cebf83d1cbcd94
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-02 14:52:53 +0200

    Finish part 10 exercise 11

--- mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
+++ mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human>{
 
     private int wage;
     private String name;
@@ -17,9 +17,13 @@ public class Human {
         return wage;
     }
 
-
     @Override
     public String toString() {
         return name + " " + wage;
+    }
+       
+    @Override
+    public int compareTo(Human human) {
+        return human.getWage() - this.wage;
     }
 }
--- mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/MainProgram.java
@@ -8,14 +8,14 @@ public class MainProgram {
         ArrayList<Human> humans = new ArrayList<>();
         humans.add(new Human("Matti", 150000));
         humans.add(new Human("Merja", 500));
-        humans.add(new Human("Pertti", 80));
+        humans.add(new Human("Pertti", 800));
 
         System.out.println(humans);
 
         /*
          * Uncomment the comment below when you have completed the compareTo-method.
          */
-        //Collections.sort(humans);
+        Collections.sort(humans);
         System.out.println(humans);
 
     }
