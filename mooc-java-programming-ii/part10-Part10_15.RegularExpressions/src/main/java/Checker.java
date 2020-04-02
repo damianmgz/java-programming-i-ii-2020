45e2ec5f7e4babe604067de3945b1c219de89a19
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-02 19:34:45 +0200

    Finish part 10 exercise 15

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -2,4 +2,19 @@
 
 public class Checker {
 
+    public Checker() {
+    }
+    
+    public boolean isDayOfWeek(String string) {
+        return (string.matches("mon|tue|wed|thu|fri|sat|sun"));
+ 
+    }
+    
+    public boolean allVowels(String string) {
+        return (string.matches("[aeiou]*"));
+    }
+    
+    public boolean timeOfDay(String string) {
+        return (string.matches("(([0-1][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]"));
+    }
 }
--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,11 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Checker check = new Checker();
+        String s = "00:54:37";
+        
+        System.out.println(check.isDayOfWeek(s));
+        System.out.println(check.allVowels(s));
+        System.out.println(check.timeOfDay(s));
     }
 }
