4dfb770a927d4199360c58dc682017f467609ea2
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-09 17:22:19 +0200

    Finish part 11 exercise 4

---
+++ mooc-java-programming-ii/part11-Part11_04.StudentAndUniversity/src/main/java/Student.java
@@ -0,0 +1,6 @@
+
+public class Student {
+    private int studentID;
+    private String name;
+    private University university;
+}
---
+++ mooc-java-programming-ii/part11-Part11_04.StudentAndUniversity/src/main/java/University.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class University {
+    private String name;
+    private ArrayList<Student> students;
+}
