7669da51628bf29577276ec240c084824cac0c78
Damian <51610356+damianmgz@users.noreply.github.com>
2020-04-11 16:44:15 +0200

    Finish part 11 exercise 11

--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,7 +3,10 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
-
+        if (num < 0) {
+            throw new IllegalArgumentException("Number must not be negative");
+        }
+        
         int answer = 1;
         for (int i = 1; i <= num; i++) {
             answer *= i;
@@ -13,7 +16,14 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
-
+        if (setSize < 0 || subsetSize < 0) {
+            throw new IllegalArgumentException("Parameters must not be negative");
+        }
+        
+        if (subsetSize > setSize) {
+            throw new IllegalArgumentException("Subset must not be greater than set");
+        }
+        
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
 
--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -6,7 +6,22 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
-
+        if (name == null) {
+            throw new IllegalArgumentException("Name must not be null");
+        }
+        
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException ("Name must not be empty");
+        }
+        
+        if (name.length() > 40) {
+            throw new IllegalArgumentException("Name length maximum 40 chars");
+        }
+                
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Age must be between 0 and 120");
+        }
+        
         this.name = name;
         this.age = age;
     }
